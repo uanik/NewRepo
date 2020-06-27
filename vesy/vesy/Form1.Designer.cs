806865d22ee08e6ad1fc688d83a1bb784db529d8
uanik <uanik.com.ua@gmail.com>
2020-06-27 14:41:30 +0300

    Добавлен проект весы для софиевки

---
+++ vesy/vesy/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 400;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label3.Location = new System.Drawing.Point(150, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox1.Location = new System.Drawing.Point(289, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Общий вес";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Visible = false;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(187, 326);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(13, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Журнал взвешиваний";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(17, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(228, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Количество взвешиваний";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label6.Location = new System.Drawing.Point(251, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(238, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(241, 160);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(241, 310);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "label9";
+            this.label9.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 431);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Программа взвешивания";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    //    private vesDataSet vesDataSet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ vesy/vesy/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort SerialPort1 = new SerialPort("COM2");
+        SerialPort SerialPort2 = new SerialPort("COM5");
+       OleDbConnection myOleDbConnection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=ves.mdb");
+      
+        static int rzr;
+        static int rzr0 = 1;
+        public Form1()
+        {
+          
+            InitializeComponent();
+            SerialPort1.BaudRate = 2400;
+            SerialPort1.Parity = Parity.None;
+            SerialPort1.StopBits = StopBits.One;
+            SerialPort1.DataBits = 8;
+            SerialPort2.BaudRate = 4800;
+            SerialPort2.Parity = Parity.None;
+            SerialPort2.StopBits = StopBits.One;
+            SerialPort2.DataBits = 8;
+            SerialPort1.Open();
+            SerialPort2.Open();
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            Greed();
+            
+        }
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          
+            int ff = SerialPort1.BytesToRead;
+            int ff1 = SerialPort2.BytesToRead;
+            SerialPort1.DiscardInBuffer();
+            System.Threading.Thread.Sleep(300); //
+            string dd = System.Convert.ToString(ff);
+            string input = SerialPort1.ReadExisting();
+
+           if (input.IndexOf("a") >= 0)
+           {
+            
+               timer1.Enabled = false;
+               timer2.Enabled = true;
+               SerialPort2.DiscardInBuffer();
+               string gg = SerialPort2.ReadExisting(); 
+               rzr = 1;
+               pictureBox1.BackColor = Color.Red;
+           }
+        }
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+            SerialPort2.DiscardInBuffer(); // очистка буфера приема
+           System.Threading.Thread.Sleep(300); // задержка на 300 мс
+           int d = SerialPort2.BytesToRead; 
+           int idx;
+            string dd = System.Convert.ToString(d);
+            double output = -1;
+            string input1 = SerialPort2.ReadExisting();
+            label7.Text = input1;
+            label1.Text = input1;
+            if (myOleDbConnection.State != System.Data.ConnectionState.Open) myOleDbConnection.Open(); 
+              if (input1.IndexOf("kg") >= 0)
+            {
+                  //парсер
+                idx = input1.IndexOf("WW");
+
+                if (idx > -1)
+                {
+                    input1 = input1.Substring(idx + 2, 9);
+                    label8.Text = input1;
+                }
+                idx = input1.IndexOf("kg");
+                if (idx == 7)
+                {
+                 
+                    input1 = input1.Substring(0, 7);
+                    label9.Text = input1;
+                    string ves = input1;
+             
+                }
+                
+                idx = input1.IndexOf('.');
+                if (idx == 5)
+                {
+                    string input_ = input1.Replace('.', ',');
+                    output = Convert.ToDouble(input_);
+                
+                }
+                if (output > 0 & output < 301 & rzr == 1 & rzr0 == 1)
+               
+                { // если вес больше 0 и меньше 300 и запись в БД разрешена 
+                    // зажечь светодиод
+                  /* --узнать текущее дату и время и преобразовать его в символьный вид ---------------*/
+                 //   label3.Text = output.ToString() + " кг";
+                   DateTime dk = DateTime.Now; //
+                   string dat = dk.ToString();
+                   string  dd1 = dat.Substring(0, 2);
+                   string  mm = dat.Substring(3, 2);
+                   string  yyyy = dat.Substring(6, 4);
+                   string  chmm = dat.Substring(11, 7);
+                   string  tek_data = "#" + yyyy + "/" + mm + "/" + dd1 + " " + chmm + "#";
+                    /* --сформировать SQL запрос вставки строки в БД -----------------*/
+                   string textCommand1 = "INSERT INTO ves(data,ves) values(" + tek_data + "," + input1 + ")";
+                    OleDbCommand command = new OleDbCommand(textCommand1, myOleDbConnection);
+                    SerialPort1.WriteLine("+"); // Записать в SerialPort1 символ "+"
+                    command.ExecuteNonQuery(); // записать данные в БД
+                    myOleDbConnection.Close();
+                    
+                    DateTime df = DateTime.Today;
+                    string tek_data1 = df.ToString();
+                    string dd2 = tek_data1.Substring(0, 2);
+                    string mm2 = tek_data1.Substring(3, 2);
+                    string yyyy2 = tek_data1.Substring(6, 4);
+                    tek_data = "#" + yyyy2 + "/" + mm2 + "/" + dd2 + "#";
+                    Greed();
+                    
+                    rzr = 0; // сбросить флаг разрешения записи в БД
+                    rzr0 = 0;
+
+                }
+                // АНАЛИЗ НА НУЛЕВОЙ ВЕС
+                if (output == 0) //ЕСЛИ ВЕС равен НУЛю  отправка "-" в порт1
+                { SerialPort1.WriteLine("-"); // потушить светодиод
+             //   SerialPort1.DiscardInBuffer();
+                timer1.Enabled = true;
+                timer2.Enabled = false;
+                rzr0 = 1;
+                pictureBox1.BackColor = Color.Lime;
+                }
+                    
+
+            }
+        }
+
+        private void Greed()
+        {
+            DateTime dk = DateTime.Now; //
+            string dat = dk.ToString();
+            string dd1 = dat.Substring(0, 2);
+            string mm = dat.Substring(3, 2);
+            string yyyy = dat.Substring(6, 4);
+            string chmm = dat.Substring(11, 7);
+         //   string tek_data = "#" + yyyy + "/" + mm + "/" + dd1 + " " + chmm + "#";
+            string tek_data = "#" + yyyy + "/" + mm + "/" + dd1+"#";
+            myOleDbConnection.Open();
+            string Zapros1 = "Select  data, ves  from ves where  data >= " + tek_data + " AND data >= " + tek_data + " order by data desc"; // ZP.StartData + " and data <= " + ZP.EndData; ORDER BY data  DESC
+            string textCommand = Zapros1;
+            OleDbCommand myOleDbCommand = new OleDbCommand(textCommand, myOleDbConnection);
+            //   label2.Text = ZP.Zapros1;
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(Zapros1, myOleDbConnection);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds, "ves");
+            dataGridView1.DataSource = ds.Tables["ves"];
+            int cont = dataGridView1.RowCount;
+            if (cont > 0)
+            {
+                double sum = 0;
+                label6.Text = cont.ToString();
+                //    dataGridView1.DataSource = ds.Tables["ves"];
+                dataGridView1.Columns[1].HeaderText = "Вес";
+                dataGridView1.Columns[0].HeaderText = "Дата";
+                for (int i = 0; i < cont; i++)
+                {
+                    sum = sum + Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                }
+                label3.Text = Convert.ToString(sum) + " кг";
+            }
+            myOleDbConnection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = true;
+
+
+            rzr = 1;
+            pictureBox1.BackColor = Color.Red;
+        }
+
+     
+
+        }
+
+     
+
+    
+    }
+
+
